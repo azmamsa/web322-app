fab3e3e1f92d3be127e707654530e3d4fcaf12ba
azmamsa <105474018+azmamsa@users.noreply.github.com>
2022-09-30 18:33:16 -0400

    initial commit

--- server.js
+++ server.js
@@ -1,92 +1,67 @@
+/*********************************************************************************
+*  WEB322 – Assignment 02
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part *  of this assignment has been copied manually or electronically from any other source 
+*  (including 3rd party web sites) or distributed to other students.
+* 
+*  Name: Abid Zuberbhai Mamsa Student ID: 158290205 Date: 28/08/22
+*
+*  Online (Cyclic) Link: https://drab-jade-brown-bear-suit.cyclic.app/
+*
+********************************************************************************/ 
 var express = require("express");
-
 var app = express();
-
-
-var parse = require("body-parser")
-
+var parse = require("body-parser");
 var HTTP_PORT = process.env.PORT || 8080;
-
-var path = require("path");
-
-var dataService = require(path.join(__dirname, "./blog-service.js"));
-
-
+var path = require('path');
+var blogService = require(path.join(__dirname, "./blog-service.js"));
 
 function onHttpStart() {
     console.log("Http server listening on: " + HTTP_PORT);
 }
 
-
-//setting default to public 
-app.use(express.static('public'));
-
+app.use(express.static("public"));
 app.use(parse.urlencoded({extended:true}));
 
 
-app.get("/", function(req, res) {
-   res.sendFile(path.join(__dirname, "/views/about.html"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname,"./views/about.html"));
 });
 
-
-//the link to about page
 app.get("/about", (req, res) => {
-    res.sendFile(path.join(__dirname, "/views/about.html"));
+    res.sendFile(path.join(__dirname,"./views/about.html"));
 });
-
-
-
-app.get("/posts", (req, res) => {
-
-    if (req.query.category) {
-        dataService.getPostsByCategory(req.query.category).then((data)=>{
-            res.json(data)
-        }).catch((err)=>{
-            res.json({message: err});
-        })
-    }
-
-    
-  
-    else
-    {
-        dataService.getAllPosts().then((data) => {
-            res.json({data});
-        }).catch((err) => {
-            res.json({message: err});
-        })
-    }
-    
-});
-
-
-app.get("/blog", function(req, res){
-    dataService.getPublishedPosts() 
-    .then( function(data){{res.json(data);}})
-    .catch( function(msg){{res.json({message: msg});}})
-});
-
-
-app.get("/categories", (req, res) => {
-    dataService.getCategories().then((data) => {
-        res.json({data});
-    }).catch((err) => {
-        res.json({message: err});
-    })
-});
-
-
-
- // the page not found send message with the status 404 and message
  
+app.get('/posts', (req, res) => {
+    blogService.getAllPosts()
+    .then((data) => res.send(data))
+    .catch((err) => res.send({message: err}))
+});
 
- app.use((req, res) => {
+app.get('/blog', (req, res) => {
+    blogService.getPublishedPosts()
+    .then((data) => res.send(data))
+    .catch((err) => res.send({message: err}))
+});
+
+app.get('/categories', (req, res) => {
+    blogService.getCategories()
+    .then((data) => res.send(data))
+    .catch((err) => res.send({message: err}))
+});
+
+
+
+
+
+
+app.use((req, res) => {
     res.status(404).sendFile(path.join(__dirname,"/views/404.html"));
-  });
+});
 
 
 
- dataService.initialize()
+ blogService.initialize()
  .then(() => {app.listen(HTTP_PORT, onHttpStart);})
- .catch(() => {console.log("error!")});
-
+ .catch(() => {console.log("error!")
+});
